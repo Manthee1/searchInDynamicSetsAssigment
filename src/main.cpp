85da4f20015131a06c29e6d165c29551be35f49a
Manthee1 <dinja.martin@gmail.com>
2023-02-25 20:25:32 +0100

    Included benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,8 @@
-// Your task in this assignment is to implement and then compare 4
-// implementations of data structures in terms of the effectiveness of insert,
-// delete and search operations in different situations:
-//
-// - (3 points) Implementation of a binary search tree (BVS) with any balancing
-// algorithm, e.g. AVL, Red-Black Trees, (2,3) Trees, (2,3,4) Trees, Splay
-// Trees, ...
-// - (3 points) Second implementation of BVS with a different balancing
-// algorithm than in the previous point.
-// - (3 points) Implementation of a hash table with collision resolution of your
-// choice. The hash table size adjustment must also be implemented.
-// - (3 points) Second implementation of the hash table with collision
-// resolution in a different way than in the previous point. The hash table size
-// adjustment must also be implemented.
-
 #include <iostream>
 #include <sstream>
 #include "gui.h"
 #include "tests/test.h"
+#include "Benchmark.h"
 #include "gui/ViewController.h"
 
 using namespace std;
@@ -27,10 +13,14 @@ int main(int argc, char** argv) {
 		string arg = string(argv[1]);
 
 		if (arg == "--test") {
-			testAVLTree();
+			testAVLTree(true);
 			// testAVLTree2();
 			// testHashTable();
 			// testHashTable2();
+			return 0;
+		}
+		if (arg == "--benchmark") {
+			Benchmark::run(AVL, 1000, 800, 500, 10);
 			return 0;
 		}
 		return 0;
