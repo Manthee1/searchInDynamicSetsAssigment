1d21e2343879512264b23d6ed3a21fe8566a8356
Manthee1 <dinja.martin@gmail.com>
2023-03-14 21:15:48 +0100

    Reworked main

--- src/main.cpp
+++ src/main.cpp
@@ -37,15 +37,29 @@ DataStructureType getDataType(string dataTypeString) {
 	return dataStrctureType;
 }
 
+enum RandomizationType getRandType(string randTypeString) {
+	// Get the randomization type
+	enum RandomizationType randomizationType = UNIQUE_RANDOM;
+
+	if (randTypeString == "random" || randTypeString == "r")
+		randomizationType = NON_UNIQUE_RANDOM;
+	else if (randTypeString != "unique" && randTypeString != "u")
+		cout << YELLOW "INFO: Invalid randomization type, using unique" RESET << endl;
+}
+
+void printTestHelp() {
+	cout << "  test [rand|fixed|gen|benchmark] - Run a test on a data structure" << endl;
+	cout << " \ttest rand [a|r|o|c] [strict|basic] [unique|random]" << endl;
+	cout << " \ttest fixed {filename} [a|r|o|c] [strict|basic]" << endl;
+	cout << " \ttest gen {filename} {keysAmount1,keysAmount2,...} [unique|random] (keysAmount is the amount of keys for each test)" << endl;
+	cout << " \ttest [benchmark|bench|b] [a|r|o|c] [strict|basic] [unique|random] - Run a benchmark on a data structure" << endl;
+}
+
 void test(int argc, char** argv) {
 	// If there is a second argument, use it as the data structure type
 	if (argc < 3) {
 		cout << "Please specify a valid test type (rand, fixed, gen)" << endl;
-		cout << "Usage:" << endl;
-		cout << " \ttest rand [a|r|o|c] [strict|basic]" << endl;
-		cout << " \ttest fixed {filename} [a|r|o|c] [strict|basic]" << endl;
-		cout << " \ttest gen {filename} {keysAmount1,keysAmount2,...} (keysAmount is the amount of keys for each test)" << endl;
-
+		printTestHelp();
 		return;
 	}
 
@@ -53,7 +67,7 @@ void test(int argc, char** argv) {
 	if (testTypeString == "rand") {
 		// If there is a third argument, use it as the data structure type
 		if (argc < 4) {
-			cout << "Please specify a data structure type (a, r, o, c)" << endl;
+			cout << "Please specify a data structure type [a|r|o|c]" << endl;
 			return;
 		}
 		string dsTypeString = string(argv[3]);
@@ -68,8 +82,16 @@ void test(int argc, char** argv) {
 		} else
 			cout << YELLOW "INFO: Test type not provided. Using basic test" RESET << endl;
 
+		// Get the randomization type
+		enum RandomizationType randomizationType = UNIQUE_RANDOM;
+		if (argc > 5) {
+			string randTypeString = string(argv[5]);
+			randomizationType = getRandType(randTypeString);
+		} else
+			cout << YELLOW "INFO: Randomization type not provided. Using unique randomization" RESET << endl;
+
 		enum DataStructureType dsType = getDataType(dsTypeString);
-		Test::run(dsType, testType);
+		Test::run(dsType, testType, randomizationType);
 		return;
 	} else if (testTypeString == "fixed") {
 		// Make sure that the file name is provided
@@ -81,7 +103,7 @@ void test(int argc, char** argv) {
 		string fileName = string(argv[3]);
 		// If there is a fourth argument, use it as the data structure type
 		if (argc < 5) {
-			cout << "Please specify a data structure type (a, r, o, c)" << endl;
+			cout << "Please specify a data structure type [a|r|o|c]" << endl;
 			return;
 		}
 		string dsTypeString = string(argv[4]);
@@ -137,69 +159,87 @@ void test(int argc, char** argv) {
 			keysAmount[keysAmountSize] = atoi(item.c_str());
 			keysAmountSize++;
 		}
+
+		// Get the randomization type
+		enum RandomizationType randomizationType = UNIQUE_RANDOM;
+		if (argc > 5) {
+			string randTypeString = string(argv[5]);
+			randomizationType = getRandType(randTypeString);
+		} else
+			cout << YELLOW "INFO: Randomization type not provided. Using unique randomization" RESET << endl;
+
 		// Generate the test file
-		Test::generateTestFile(fileName, keysAmount, keysAmountSize);
+		Test::generateTestFile(fileName, randomizationType, keysAmount, keysAmountSize);
+		return;
+	} else if (testTypeString == "benchmark" || testTypeString == "bench" || testTypeString == "b") {
+		// Make sure there is a benchmark type
+		if (argc < 4) {
+			cout << "Please specify a benchmark type [a|r|o|c]" << endl;
+			printTestHelp();
+			return;
+		}
+
+		// Get the benchmark type
+		string benchmarkTypeString = string(argv[3]);
+		enum DataStructureType benchmarkType = getDataType(benchmarkTypeString);
+
+		// Get the type of randomization
+		//  unique/u: all the keys are unique
+		//  random/r: the keys are random
+		string randomizationTypeString = string(argv[4]);
+		enum RandomizationType randomizationType = UNIQUE_RANDOM;
+		if ((argc > 4) && (randomizationTypeString == "random" || randomizationTypeString == "r"))
+			randomizationType = NON_UNIQUE_RANDOM;
+
+		// Get the amount of elements
+		int elementsAmount = (argc > 5) ? atoi(argv[5]) : 1000;
+		if (argc <= 5) cout << YELLOW "INFO: Amount of elements not provided. Using" << elementsAmount << RESET << endl;
+
+		Benchmark::verboseLevel = 2;
+		Benchmark::run(benchmarkType, randomizationType, elementsAmount, elementsAmount, elementsAmount, 10);
 		return;
 	}
-
 	cout << "Invalid test type provided" << endl;
 	return;
 }
 
-void benchmark(int argc, char** argv) {
-	// Make sure there is a second argument (the data structure type)
-	if (argc < 3) {
-		cout << "Please specify a benchmark type (a, r, o, c)" << endl;
-		return;
-	}
-
-	// Get the benchmark type
-	string benchmarkTypeString = string(argv[2]);
-	enum DataStructureType benchmarkType = getDataType(benchmarkTypeString);
-
-	// Get the type of randomization
-	//  unique/u: all the keys are unique
-	//  random/r: the keys are random
-	string randomizationTypeString = string(argv[3]);
-	enum RandomizationType randomizationType = UNIQUE_RANDOM;
-	if ((argc > 3) && (randomizationTypeString == "random" || randomizationTypeString == "r"))
-		randomizationType = NON_UNIQUE_RANDOM;
-
-	// Get the amount of elements
-	int elementsAmount = (argc > 4) ? atoi(argv[3]) : 1000;
-
-	Benchmark::verboseLevel = 2;
-	Benchmark::run(benchmarkType, randomizationType, elementsAmount, elementsAmount, elementsAmount, 10);
-
-	return;
+void printHelp(string command) {
+	cout << "Usage: " << command << " [run|test|benchmark]" << endl;
+	cout << "Options:" << endl;
+	cout << "  run [a|r|o|c]: run the GUI with the specified data structure type" << endl;
+	cout << endl;
+	printTestHelp();
 }
 
-// Add --test to the command line to run the tests
 int main(int argc, char** argv) {
 	if (argc > 1) {
 		string arg = string(argv[1]);
 
+		if (arg == "--help" || arg == "-h") {
+			printHelp(string(argv[0]));
+		}
+
 		// If arg is run, run the GUI
-		if (arg == "run") {
+		else if (arg == "run") {
 			// If there is a second argument, use it as the data structure type
 			int viewType = AVL;
 			if (argc < 3) {
-				cout << "Please specify a data structure type (a, r, o, c)" << endl;
+				cout << "Please specify a data structure type [a|r|o|c]" << endl;
 				cout << "Defaulting to AVL tree" << endl;
 			} else
 				viewType = getDataType(string(argv[2]));
 
 			ViewController::run((ViewType)viewType);
-		} else if (arg == "test")
+		} else if (arg == "test" || arg == "t")
 			test(argc, argv);
-		else if (arg == "benchmark")
-			benchmark(argc, argv);
 		else {
 			cout << "Invalid argument provided" << endl;
-			cout << "Usage: " << argv[0] << " [run|test|benchmark]" << endl;
+			printHelp(string(argv[0]));
 		}
-	} else
-		ViewController::run();
+	} else {
+		cout << "No argument provided" << endl;
+		printHelp(string(argv[0]));
+	}
 
 	// Delete WrappedDS::DSentries
 	for (int i = 0; i < 4; i++)
